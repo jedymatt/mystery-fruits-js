a2bc95cd53510ce65dfa94896df89ad8b9d18f7c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-06 00:59:33 +0800

    Added index badge when selected

--- src/components/FruitButton.vue
+++ src/components/FruitButton.vue
@@ -11,30 +11,28 @@ const props = defineProps({
 
 const imageUrl = new URL(`../assets/images/${props.fruit}.png`, import.meta.url).href
 
-function getIndex(index) {
-  return index !== -1 ? index + 1 : null;
+function getIndex() {
+  return props.selectedIndex + 1;
+}
+
+function isSelected() {
+  return props.selectedIndex !== -1;
 }
 
 </script>
 
 <template>
-  <div class="flex flex-col gap-1 items-center p-2 hover:cursor-pointer hover:bg-gray-100 rounded-lg"
-  :class="[{
-    'bg-gray-100 border': selectedIndex !== -1,
-  }]">
-    <img :src="imageUrl" :alt="`${fruit} image`" class="h-16 w-16 md:h-32 md:w-32">
-    <div class="text-xs uppercase font-semibold">
-      {{ fruit }}
+    <div class="relative flex flex-col gap-1 p-2 items-center hover:cursor-pointer hover:bg-gray-100 rounded-lg"
+      :class="{
+        'bg-gray-200': isSelected(),
+      }">
+      <img :src="imageUrl" :alt="`${fruit} image`" class="h-16 w-16 md:h-32 md:w-32">
+      <div class="text-xs uppercase font-semibold">
+        {{ fruit }}
+      </div>
+      <div v-if="isSelected()"
+        class="absolute top-2 right-2 text-gray-700 border-2 border-gray-400 rounded-full p-0.5 flex items-center justify-center h-6 w-6">
+        <span class="text-xs font-medium">{{ getIndex(selectedIndex) }}</span>
+      </div>
     </div>
-    <div>
-      <span :class="[
-          {
-            'text-gray-700 border-2 rounded-full p-1 text-sm': selectedIndex !== -1,
-          }
-      ]"
-      class="absolute bottom-0 right-0">
-        {{ getIndex(selectedIndex) }}
-      </span>
-    </div>
-  </div>
 </template>
