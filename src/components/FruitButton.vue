096312713ee933e29c90ab368891d33133e93f8b
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-06 17:29:50 +0800

    add justify-center

--- src/components/FruitButton.vue
+++ src/components/FruitButton.vue
@@ -22,7 +22,7 @@ function isSelected() {
 </script>
 
 <template>
-  <button class="relative flex flex-col gap-1 p-2 items-center hover:cursor-pointer rounded-lg"
+  <button class="relative flex flex-col gap-1 p-2 items-center justify-center hover:cursor-pointer rounded-lg"
     :class="[isSelected() ? 'bg-gray-200' : 'hover:bg-gray-100']">
     <img :src="imageUrl" :alt="`${fruit} image`" class="h-16 w-16 md:h-32 md:w-32">
     <div class="text-xs uppercase font-semibold">
