7b1fdb9017c4f67eb1b5e8df0590af71d1109bb6
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-06 17:41:09 +0800

    Shift input and history to right side

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -72,61 +72,38 @@ onMounted(() => {
 
 <template>
     <main>
-        <div class="container md:p-12 p-2">
-            <div class="grid grid-cols-3 gap-6">
-                <FruitButton v-for="fruit in fruits" :key="fruit" :fruit="fruit" @click="selectFruit(fruit)"
-                    :selected-index="getSelectedFruitIndex(fruit)" />
-            </div>
+        <div class="md:p-12 p-2">
+            <div class="lg:flex lg:flex-row lg:gap-6">
+                <div class="grid grid-cols-3 gap-6">
+                    <FruitButton v-for="fruit in fruits" :key="fruit" :fruit="fruit" @click="selectFruit(fruit)"
+                        :selected-index="getSelectedFruitIndex(fruit)" />
+                </div>
 
-            <div class="mt-4">
-                <div class="rounded-md border border-gray-200 md:p-12 p-2">
-                    <div class="grid grid-cols-3 gap-6">
-                        <button @click="unselectFruit(fruit)" v-for="fruit in selectedFruits" :key="fruit" :class="[
-                            'bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded-lg',
-                            {
-                                'bg-red-300': selectedFruits.length === 3 && !isCorrectAnswer(),
-                                'bg-green-300': selectedFruits.length === 3 && isCorrectAnswer(),
-                            },
-                        ]">
-                            {{ fruit }}
-                        </button>
+                <div>
+                    <div class="mt-4">
+                        <div class="rounded-md border border-gray-200 md:p-12 p-2">
+                            <div class="grid grid-cols-3 gap-6">
+                                <button @click="unselectFruit(fruit)" v-for="fruit in selectedFruits" :key="fruit"
+                                    :class="[
+                                        'bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded-lg',
+                                        {
+                                            'bg-red-300': selectedFruits.length === 3 && !isCorrectAnswer(),
+                                            'bg-green-300': selectedFruits.length === 3 && isCorrectAnswer(),
+                                        },
+                                    ]">
+                                    {{ fruit }}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-4">
+                        Remaining attempts: {{ availableAttempts }}
+                    </div>
+                    <div class="mt-4">
+                        <HistorySection :history="history" />
                     </div>
                 </div>
             </div>
-            <div class="mt-4">
-                Remaining attempts: {{ availableAttempts }}
-            </div>
-            <div class="mt-4">
-                <HistorySection :history="history" />
-            </div>
-
-            <span id="credits" class="text-3xl font-bold">Credits:</span>
-            <ul class="list-disc list-inside">
-                <li>
-                    <a href="https://www.flaticon.com/free-icons/fruit" title="fruit icons">Fruit icons created by
-                        Smashicons -
-                        Flaticon</a>
-                </li>
-                <li>
-                    <a href="https://www.flaticon.com/free-icons/kiwi" title="kiwi icons">Kiwi icons created by AngDiz -
-                        Flaticon</a>
-                </li>
-                <li>
-                    <a href="https://www.flaticon.com/free-icons/peanut" title="peanut icons">Peanut icons created by
-                        wanicon -
-                        Flaticon</a>
-                </li>
-                <li>
-                    <a href="https://www.flaticon.com/free-icons/mango" title="mango icons">Mango icons created by
-                        max.icons -
-                        Flaticon</a>
-                </li>
-                <li>
-                    <a href="https://www.flaticon.com/free-icons/papaya" title="papaya icons">Papaya icons created by
-                        Freepik -
-                        Flaticon</a>
-                </li>
-            </ul>
         </div>
     </main>
 </template>
