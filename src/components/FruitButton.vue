821a13d68f6ad71be4237d7b1320451a696c8228
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-06 12:09:20 +0800

    Change div to button

--- src/components/FruitButton.vue
+++ src/components/FruitButton.vue
@@ -22,7 +22,7 @@ function isSelected() {
 </script>
 
 <template>
-  <div class="relative flex flex-col gap-1 p-2 items-center hover:cursor-pointer rounded-lg"
+  <button class="relative flex flex-col gap-1 p-2 items-center hover:cursor-pointer rounded-lg"
     :class="[isSelected() ? 'bg-gray-200' : 'hover:bg-gray-100']">
     <img :src="imageUrl" :alt="`${fruit} image`" class="h-16 w-16 md:h-32 md:w-32">
     <div class="text-xs uppercase font-semibold">
@@ -32,5 +32,5 @@ function isSelected() {
       class="absolute top-2 right-2 text-gray-700 border border-gray-400 rounded-full flex items-center justify-center h-5 w-5 bg-white">
       <span class="text-xs font-medium text-gray-700">{{ getSelectedIndex() }}</span>
     </div>
-  </div>
+  </button>
 </template>
