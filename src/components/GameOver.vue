4d17fcb58a84502b2c59c80624006c4b8e29892c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-10 20:24:07 +0800

    Added GameOver component

---
+++ src/components/GameOver.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+    answer: {
+        type: Array,
+    },
+})
+
+function restartGame() {
+    // TODO restart game
+}
+</script>
+
+<template>
+    <div>
+        <h1>Game Over</h1>
+        <p>You have no more attempts left.</p>
+        <p>The correct answer was:</p>
+        <ul>
+            <li v-for="fruit in answer" :key="fruit">{{ fruit }}</li>
+        </ul>
+        <button @click="restartGame()">Restart Game</button>
+    </div>
+</template>
