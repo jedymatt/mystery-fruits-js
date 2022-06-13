cfd938297adeef2d8068a200ca63b3c224a0442c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-13 19:51:31 +0800

    Added comma

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: HomeView
+            component: HomeView,
         },
         {
             path: '/credits',
