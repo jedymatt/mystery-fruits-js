6642ecdeae26e832462fa0098670156f5ecbfb3d
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-13 19:56:06 +0800

    Create FruitImage.vue

---
+++ src/components/FruitImage.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+    fruit: {
+        type: String,
+    },
+});
+
+const imageUrl = new URL(`../assets/images/${props.fruit}.png`, import.meta.url).href;
+
+</script>
+
+<template>
+    <div class="p-4 rounded-md border flex flex-col gap-2 items-center justify-center overflow-clip">
+        <img :src="imageUrl" :alt="fruit" class="h-16 w-16 md:h-32 md:w-32" />
+        <span class="uppercase text-sm font-semibold text-gray-700">{{ fruit }}</span>
+    </div>
+</template>
