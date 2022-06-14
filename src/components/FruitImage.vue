ec5474b706e05de13976c0b54eca56dff43f1c14
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-14 16:19:13 +0800

    Update FruitImage.vue

--- src/components/FruitImage.vue
+++ src/components/FruitImage.vue
@@ -12,6 +12,6 @@ const imageUrl = new URL(`../assets/images/${props.fruit}.png`, import.meta.url)
 <template>
     <div class="p-4 rounded-md border flex flex-col gap-2 items-center justify-center overflow-clip">
         <img :src="imageUrl" :alt="fruit" class="h-16 w-16 md:h-32 md:w-32" />
-        <span class="uppercase text-sm font-semibold text-gray-700">{{ fruit }}</span>
+        <span class="uppercase text-sm font-semibold text-gray-800">{{ fruit }}</span>
     </div>
 </template>
