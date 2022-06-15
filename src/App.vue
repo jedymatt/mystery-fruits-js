83a2f990b6f90d8c3ab30ded406f85faf4d4afc9
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-15 15:50:50 +0800

    Add transition when navigating to different pages

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,24 @@ import AppNavbar from '@/components/AppNavbar.vue'
   <div class="min-h-screen antialiased">
     <AppNavbar />
 
-    <RouterView />
+    <!-- <RouterView /> -->
+    <RouterView v-slot="{ Component }">
+      <Transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
+
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
