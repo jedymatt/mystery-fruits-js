9aac48cd10b52339dbbe5040aba8ef8227825c58
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-16 23:10:31 +0800

    Create game.js

---
+++ src/composables/game.js
@@ -0,0 +1,66 @@
+import { ref } from "vue";
+import { useFruits } from "./fruits";
+import { useHistory } from "./history";
+
+import { countMatchingArray, countMatchingArrayOrder } from "../lib/counter";
+import { computed } from "@vue/reactivity";
+
+export function useGame() {
+  const { randomFruits, hiddenFruits, resetFruits } = useFruits();
+  const { history, clearHistory, addToHistory } = useHistory();
+
+  const remainingGuessAttempts = ref(6);
+  const selectedFruits = ref([]);
+
+  const isGameOver = computed(function () {
+    return (
+      remainingGuessAttempts.value <= 0 ||
+      countMatchingArrayOrder(selectedFruits.value, hiddenFruits.value) ===
+        hiddenFruits.value.length
+    );
+  });
+
+  function restart() {
+    resetFruits();
+    clearHistory();
+    remainingGuessAttempts.value = 6;
+  }
+
+  function checkSelectedFruits() {
+    if (isGameOver.value) {
+      return;
+    }
+
+    const matchingFruits = countMatchingArray(
+      selectedFruits.value,
+      hiddenFruits.value
+    );
+
+    const matchingFruitsOrder = countMatchingArrayOrder(
+      selectedFruits.value,
+      hiddenFruits.value
+    );
+
+    addToHistory({
+      selectedFruits: selectedFruits.value,
+      matchingFruits,
+      matchingFruitsOrder,
+      remainingGuessAttempts: remainingGuessAttempts.value,
+    });
+
+    remainingGuessAttempts.value -= 1;
+  }
+
+  return {
+    randomFruits,
+    hiddenFruits,
+    selectedFruits,
+    history,
+    addToHistory,
+    clearHistory,
+    remainingGuessAttempts,
+    isGameOver,
+    restart,
+    checkSelectedFruits,
+  };
+}
