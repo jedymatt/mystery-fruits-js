09ebd192bbb7347a0abf0a061506c3adcdf64a28
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-16 13:38:16 +0800

    Add icon beside Game Over

--- src/views/GameOverView.vue
+++ src/views/GameOverView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import PrimaryButton from '@/components/PrimaryButton.vue';
 import FruitImage from '@/components/FruitImage.vue';
+import TrophyIconVue from '../components/icons/TrophyIcon.vue';
+import HeartCrackIconVue from '../components/icons/HeartCrackIcon.vue';
 
 const props = defineProps({
     answer: {
@@ -18,12 +20,22 @@ const props = defineProps({
     },
 });
 
-const remark = props.attemptsLeft === 0 ? 'You have no more attempts left.' : `You guessed it all with ${props.attemptsLeft} remaining attempts.`;
+const remark = props.attemptsLeft > 0 ? `You guessed it all with ${props.attemptsLeft} remaining attempts.` : 'You have no more attempts left.';
+
+function isSuccess() {
+    return props.attemptsLeft > 0;
+}
 </script>
 
 <template>
     <div class="flex flex-col items-center justify-center">
-        <h1 class="text-pink-500">Game Over</h1>
+        <h1 class="text-pink-500 inline-flex items-center gap-1">
+            Game Over
+            <div class="h-auto w-8 hover:animate-bounce">
+                <TrophyIconVue v-if="isSuccess()" />
+                <HeartCrackIconVue v-else />
+            </div>
+        </h1>
         <p class="text-gray-800">{{ remark }}</p>
         <p class="text-gray-800">The correct answer is:</p>
 
