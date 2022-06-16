c5c8c4499b76ee26df5c97d25c9ae5c24f0e4bae
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-16 23:10:02 +0800

    Update fruits.js

--- src/lib/fruits.js
+++ src/lib/fruits.js
@@ -1,40 +1,40 @@
-const fruits = [
-    'apple',
-    'banana',
-    'orange',
-    'strawberry',
-    'watermelon',
-    'kiwi',
-    'grape',
-    'cherry',
-    'peach',
-    'pineapple',
-    'mango',
-    'papaya',
-    'coconut',
-    'avocado',
-    'peanut',
-]
+export const fruits = [
+  "apple",
+  "banana",
+  "orange",
+  "strawberry",
+  "watermelon",
+  "kiwi",
+  "grape",
+  "cherry",
+  "peach",
+  "pineapple",
+  "mango",
+  "papaya",
+  "coconut",
+  "avocado",
+  "peanut",
+];
 
-export function randomUniqueFruits(length) {
-    const randomFruits = []
-    while (randomFruits.length < length) {
-        const randomIndex = Math.floor(Math.random() * fruits.length)
-        const randomFruit = fruits[randomIndex]
-        if (!randomFruits.includes(randomFruit)) {
-            randomFruits.push(randomFruit)
-        }
+export function randomUniqueFruits(length = 6) {
+  const randomFruits = [];
+  while (randomFruits.length < length) {
+    const randomIndex = Math.floor(Math.random() * fruits.length);
+    const randomFruit = fruits[randomIndex];
+    if (!randomFruits.includes(randomFruit)) {
+      randomFruits.push(randomFruit);
     }
-    return randomFruits
+  }
+  return randomFruits;
 }
 
-export function getRandomUniqueFrom(array, length) {
-    const randomIndexes = []
-    while (randomIndexes.length < length) {
-        const randomIndex = Math.floor(Math.random() * array.length)
-        if (!randomIndexes.includes(randomIndex)) {
-            randomIndexes.push(randomIndex)
-        }
+export function getRandomUniqueFrom(array, length = 3) {
+  const randomIndexes = [];
+  while (randomIndexes.length < length) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    if (!randomIndexes.includes(randomIndex)) {
+      randomIndexes.push(randomIndex);
     }
-    return randomIndexes.map(index => array[index])
+  }
+  return randomIndexes.map((index) => array[index]);
 }
