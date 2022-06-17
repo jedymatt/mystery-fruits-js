368e815546a586754ca9753bb12f684230f1d780
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-17 22:47:26 +0800

    Added plugin:vue/vue3-recommended

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
   "extends": [
     "plugin:vue/vue3-essential",
     "plugin:vue/vue3-strongly-recommended",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended"
   ]
 }
