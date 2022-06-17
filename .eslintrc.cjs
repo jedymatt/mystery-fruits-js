aaf7800d386fe656cc13c71488c8e81c72bf287b
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-17 12:47:24 +0800

    Removed env

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,5 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "plugin:vue/vue3-strongly-recommended",
     "eslint:recommended"
-  ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
+  ]
 }
