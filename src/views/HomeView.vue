88ca8d9a4deb2078c83aa5823cc82f4d0659a3e8
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-18 20:12:20 +0800

    Added isLoading and disable FruitButton when Loading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,93 +1,99 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted, computed } from "vue";
 
-import { countCorrectFruits, countCorrectFruitsOrder } from '@/lib/functions.js';
-import HistorySection from '@/components/HistorySection.vue';
-import FruitButton from '@/components/FruitButton.vue';
-import GameOverView from '@/views/GameOverView.vue';
-import { useFruits } from '../composables/fruits';
-import { useHistory } from '../composables/history';
+import {
+  countCorrectFruits,
+  countCorrectFruitsOrder,
+} from "@/lib/functions.js";
+import HistorySection from "@/components/HistorySection.vue";
+import FruitButton from "@/components/FruitButton.vue";
+import GameOverViewVue from "./GameOverView.vue";
+import { useFruits } from "../composables/fruits";
+import { useHistory } from "../composables/history";
 
 const { hiddenFruits, randomFruits, resetFruits } = useFruits();
 const { history, addToHistory, clearHistory } = useHistory();
 
-const availableAttempts = ref(6)
-const selectedFruits = ref([])
-const isGameOver = ref(false)
+const availableAttempts = ref(6);
+const selectedFruits = ref([]);
+const isGameOver = computed(() => {
+  return (
+    availableAttempts.value === 0 ||
+    countCorrectFruitsOrder(selectedFruits.value, hiddenFruits.value) ===
+    hiddenFruits.value.length
+  );
+});
+const isLoading = ref(false);
 
 function selectFruit(fruit) {
+  if (isLoading.value || isGameOver.value) return;
+
   if (selectedFruits.value.length < 3) {
-    if (selectedFruits.value.includes(fruit)) return
-    selectedFruits.value.push(fruit)
+    if (selectedFruits.value.includes(fruit)) return;
+    selectedFruits.value.push(fruit);
   }
 
   if (selectedFruits.value.length === 3) {
-    setTimeout(() => {
-      checkAnswer();
+    isLoading.value = true;
+    setTimeout(function () {
       addSelectedFruitsToHistory();
       clearSelectedFruits();
-    }, 500);
+      availableAttempts.value--;
+      isLoading.value = false;
+    }, 400);
   }
 }
 
 function clearSelectedFruits() {
-  selectedFruits.value = []
+  selectedFruits.value = [];
 }
 
 function unselectFruit(fruit) {
-  selectedFruits.value = selectedFruits.value.filter(f => f !== fruit)
-}
-
-function checkAnswer() {
-  const correct = isCorrectAnswer()
-  availableAttempts.value--
-
-  if (correct) {
-    selectedFruits.value = []
-    history.value = []
-    isGameOver.value = true
-  }
-
-  if (availableAttempts.value <= 0) {
-    isGameOver.value = true
-  }
-
+  selectedFruits.value = selectedFruits.value.filter((f) => f !== fruit);
 }
 
 function isCorrectAnswer() {
-  const correctFruits = countCorrectFruits(hiddenFruits.value, selectedFruits.value)
-  const correctFruitsOrder = countCorrectFruitsOrder(hiddenFruits.value, selectedFruits.value)
-  return correctFruits === 3 && correctFruitsOrder === 3
+  const correctFruits = countCorrectFruits(
+    hiddenFruits.value,
+    selectedFruits.value
+  );
+  const correctFruitsOrder = countCorrectFruitsOrder(
+    hiddenFruits.value,
+    selectedFruits.value
+  );
+  return correctFruits === 3 && correctFruitsOrder === 3;
 }
 
 function addSelectedFruitsToHistory() {
   addToHistory({
     selectedFruits: selectedFruits.value,
     correctFruits: countCorrectFruits(hiddenFruits.value, selectedFruits.value),
-    correctFruitsOrder: countCorrectFruitsOrder(hiddenFruits.value, selectedFruits.value),
-  })
+    correctFruitsOrder: countCorrectFruitsOrder(
+      hiddenFruits.value,
+      selectedFruits.value
+    ),
+  });
 }
 
 function getSelectedFruitIndex(fruit) {
-  return selectedFruits.value.indexOf(fruit)
+  return selectedFruits.value.indexOf(fruit);
 }
-
 
 function restartGame() {
   resetFruits();
 
-  selectedFruits.value = []
-  availableAttempts.value = 6
+  selectedFruits.value = [];
+  availableAttempts.value = 6;
   clearHistory();
-  isGameOver.value = false
-  console.log(`Answer: ${hiddenFruits.value}`)
+  // isGameOver.value = false
+  console.log(`Answer: ${hiddenFruits.value}`);
 }
 
 onMounted(() => {
-  console.log(`Answer: ${hiddenFruits.value}`)
+  console.log(`Answer: ${hiddenFruits.value}`);
 });
 
-const reversedHistory = computed(() => history.value.slice().reverse())
+const reversedHistory = computed(() => history.value.slice().reverse());
 </script>
 
 <template>
@@ -103,6 +109,7 @@ const reversedHistory = computed(() => history.value.slice().reverse())
             :key="fruit"
             :fruit="fruit"
             :selected-index="getSelectedFruitIndex(fruit)"
+            :is-disabled="isLoading.value"
             @click="selectFruit(fruit)"
           />
         </div>
@@ -117,8 +124,10 @@ const reversedHistory = computed(() => history.value.slice().reverse())
                   :class="[
                     'bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded-lg',
                     {
-                      'bg-red-300': selectedFruits.length === 3 && !isCorrectAnswer(),
-                      'bg-green-300': selectedFruits.length === 3 && isCorrectAnswer(),
+                      'bg-red-300':
+                        selectedFruits.length === 3 && !isCorrectAnswer(),
+                      'bg-green-300':
+                        selectedFruits.length === 3 && isCorrectAnswer(),
                     },
                   ]"
                   @click="unselectFruit(fruit)"
@@ -137,9 +146,9 @@ const reversedHistory = computed(() => history.value.slice().reverse())
         </div>
       </div>
       <div v-if="isGameOver">
-        <GameOverView
-          :answer="hiddenFruits"
+        <GameOverViewVue
           :restart-game="restartGame"
+          :answer="hiddenFruits"
           :attempts-left="availableAttempts"
         />
       </div>
