8d886981dd4dbc54913d20c9191f17163140835b
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-19 11:33:38 +0800

    Update HistorySection

--- src/components/HistorySection.vue
+++ src/components/HistorySection.vue
@@ -1,45 +1,87 @@
 <script setup>
+import FruitImage from './FruitImage.vue';
+
 defineProps({
-    history: {
-        type: Array,
-        default: () => []
-    },
+  history: {
+    type: Array,
+    default: () => []
+  },
 })
 </script>
 
 <template>
-  <div class="container">
-    <span class="text-xl font-black">History:</span>
-    <table class="w-full">
-      <thead>
-        <tr class="text-left">
-          <th colspan="3">
-            Selected fruits
-          </th>
-          <th>Correct fruits</th>
-          <th>Correct order</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in history"
-          :key="item"
-          class="border-b"
+  <div class="w-full">
+    <h2 class="text-pink-500">
+      History
+    </h2>
+    <div class="p-2 border rounded-md">
+      <div
+        v-for="items in history"
+        :key="items"
+      >
+        <div class="grid grid-cols-2">
+          <div class="flex flex-row">
+            <div
+              v-for="fruit in items.selectedFruits"
+              :key="fruit"
+              class="flex flex-col items-center justify-center gap-1"
+            >
+              <FruitImage
+                :fruit="fruit"
+                class="h-8 w-8"
+              />
+              <span class="text-xs capitalize">
+                {{ fruit }}
+              </span>
+            </div>
+          </div>
+          <div class="flex flex-row">
+            <div class="flex flex-col items-center justify-center">
+              <div class="h-8 w-8">
+                {{ items.correctFruits }}
+              </div>
+              <span class="text-xs">Correct Fruits</span>
+            </div>
+            <div class="flex flex-col items-center justify-center">
+              <div class="h-8 w-8">
+                {{ items.correctFruitsOrder }}
+              </div>
+              <span class="text-xs">Correct Fruits Order</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- <div
+        v-for="items in history"
+        :key="items"
+        class="flex flex-row items-center justify-center gap-2 py-2 border-b last:border-b-0"
+      >
+        <div
+          v-for="fruit in items.selectedFruits"
+          :key="fruit"
+          class="flex flex-col items-center justify-center gap-1"
         >
-          <td
-            v-for="fruit in item.selectedFruits"
-            :key="fruit"
-          >
+          <FruitImage
+            :fruit="fruit"
+            class="h-8 w-8"
+          />
+          <span class="text-xs capitalize">
             {{ fruit }}
-          </td>
-          <td>
-            {{ item.correctFruits }}
-          </td>
-          <td>
-            {{ item.correctFruitsOrder }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          </span>
+        </div>
+        <div class="flex flex-col items-center justify-center">
+          <div class="h-8 w-8">
+            {{ items.correctFruits }}
+          </div>
+          <span class="text-xs">Correct Fruits</span>
+        </div>
+        <div class="flex flex-col items-center justify-center">
+          <div class="h-8 w-8">
+            {{ items.correctFruitsOrder }}
+          </div>
+          <span class="text-xs">Correct Fruits Order</span>
+        </div>
+      </div> -->
+    </div>
   </div>
 </template>
