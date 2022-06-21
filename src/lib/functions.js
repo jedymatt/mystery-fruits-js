7b36e17eebc0aba973e1442b2caf1555741a0f2b
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-21 21:22:13 +0800

    Delete functions.js

--- src/lib/functions.js
+++
@@ -1,7 +0,0 @@
-export function countCorrectFruits(fruits, guesses) {
-    return guesses.filter(guess => fruits.includes(guess)).length
-}
-
-export function countCorrectFruitsOrder(fruits, guesses) {
-    return guesses.filter((guess, index) => fruits[index] === guess).length
-}
