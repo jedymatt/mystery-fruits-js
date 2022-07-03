baf4d2b855b392a315de5cf8e33e93608ad56bd6
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-03 21:59:24 +0800

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,6 @@
 module.exports = {
   "root": true,
   "extends": [
-    "plugin:vue/vue3-essential",
-    "plugin:vue/vue3-strongly-recommended",
     "plugin:vue/vue3-recommended",
     "eslint:recommended"
   ]
