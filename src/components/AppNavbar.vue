2dcea3689d00366d179b07ff5ef9eef3ce1accb4
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-07 16:32:47 +0800

    Revert "Fixed the issue where this branch revert master changes"
    This reverts commit 18623a8b9a72f2311c443a873657206682a8145c, reversing
    changes made to 03f4535416325f5862f3c3b801de93fac52d082d.

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -26,8 +26,16 @@ const toggleDarkMode = () => {
           <RouterLink
             to="/credits"
             class="p-2 rounded-md hover:bg-pink-100"
+            :class="{ 'text-pink-600': $route.name === 'credits' }"
           >
             Credits
+          </RouterLink>
+          <RouterLink
+            to="/contributors"
+            class="p-2 rounded-md hover:bg-pink-100"
+            :class="{ 'text-pink-600': $route.name === 'contributors' }"
+          >
+            Contributors
           </RouterLink>
         </div>
         <div class="flex flex-row gap-2">
