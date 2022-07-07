03f4535416325f5862f3c3b801de93fac52d082d
arkmadj <ahmadjinadu@gmail.com>
2022-07-07 09:11:45 +0100

    Fixed the issue where this branch revert master changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,6 +15,7 @@ const availableAttempts = ref(6);
 const selectedFruits = ref([]);
 const isGameOver = ref(false);
 const isLoading = ref(false);
+const showInstructions = ref(true);
 
 function selectFruit(fruit) {
 	if (isLoading.value || isGameOver.value) return;
@@ -96,18 +97,31 @@ const reversedHistory = computed(() => history.value.slice().reverse());
 </script>
 
 <template>
-	<main class="md:p-12 p-2">
-		<div class="mb-10">
+	<main class="md:p-12 p-2 relative">
+		<div
+			:class="`mb-10 bg-red-100 inline-block p-7 border-2 border-red-300 rounded-lg relative ${
+				showInstructions ? '' : 'animate-close-section'
+			} overflow-hidden`"
+		>
+			<div
+				class="bg-red-400 p-1 rounded-full h-6 w-6 flex items-center justify-center text-red-900 absolute top-3 right-3 hover:bg-red-900 hover:text-red-400 cursor-pointer transition-all delay-75 ease-in"
+				@click="showInstructions = !showInstructions"
+			>
+				✕
+			</div>
+
 			<p>Hello Stranger 🕵️‍♂️</p>
 			<p>
-				Are you in search of the mystery fruits🍓? You’ve come to the right place🌴.
-				All you have to do is select the mystery fruits in the correct order 1️⃣ 2️⃣ 3️⃣.
+				Are you in search of the mystery fruits🍓? You’ve come to the right
+				place🌴. All you have to do is select the mystery fruits in the correct
+				order 1️⃣ 2️⃣ 3️⃣.
 			</p>
 			<p>
 				You have 6 attempts before the fruits are lost forever😞. You can check
-				the history tab on the left to see details on your previous selections⌛.
+				the history tab on the left to see details on your previous
+				selections⌛.
 			</p>
-      <p>Good luck and Have fun🌟.</p>
+			<p>Good luck and Have fun🌟.</p>
 		</div>
 		<div
 			v-if="!isGameOver"
