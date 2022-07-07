dd77930fbaaca7a66b929d992a825269c87a7cea
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-07 16:46:43 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -97,76 +97,79 @@ const reversedHistory = computed(() => history.value.slice().reverse());
 </script>
 
 <template>
-	<main class="md:p-12 p-2 relative">
-		<div
-			:class="`mb-10 bg-red-100 inline-block p-7 border-2 border-red-300 rounded-lg relative ${
-				showInstructions ? '' : 'animate-close-section'
-			} overflow-hidden`"
-		>
-			<div
-				class="bg-red-400 p-1 rounded-full h-6 w-6 flex items-center justify-center text-red-900 absolute top-3 right-3 hover:bg-red-900 hover:text-red-400 cursor-pointer transition-all delay-75 ease-in"
-				@click="showInstructions = !showInstructions"
-			>
-				✕
-			</div>
+  <main class="md:p-12 p-2 relative">
+    <div
+      :class="`mb-10 bg-red-100 inline-block p-7 border-2 border-red-300 rounded-lg relative ${
+        showInstructions ? '' : 'animate-close-section'
+      } overflow-hidden`"
+    >
+      <div
+        class="bg-red-400 p-1 rounded-full h-6 w-6 flex items-center justify-center text-red-900 absolute top-3 right-3 hover:bg-red-900 hover:text-red-400 cursor-pointer transition-all delay-75 ease-in"
+        @click="showInstructions = !showInstructions"
+      >
+        ✕
+      </div>
 
-			<p>Hello Stranger 🕵️‍♂️</p>
-			<p>
-				Are you in search of the mystery fruits🍓? You’ve come to the right
-				place🌴. All you have to do is select the mystery fruits in the correct
-				order 1️⃣ 2️⃣ 3️⃣.
-			</p>
-			<p>
-				You have 6 attempts before the fruits are lost forever😞. You can check
-				the history tab on the left to see details on your previous
-				selections⌛.
-			</p>
-			<p>Good luck and Have fun🌟.</p>
-		</div>
-		<div
-			v-if="!isGameOver"
-			id="game"
-			class="grid lg:grid-cols-2 place-items-start justify-items-center content-center gap-4"
-		>
-			<div class="h-full w-full">
-				<div class="mt-4 text-pink-500 font-semibold">
-					Remaining Attempts:
-					<span class="font-black">{{ availableAttempts }}</span>
-				</div>
-				<div class="mt-4 grid grid-cols-3 gap-2 place-items-center">
-					<div v-for="fruit in randomFruits" :key="fruit">
-						<FruitButton
-							:fruit="fruit"
-							:selected-index="getSelectedFruitIndex(fruit)"
-							:disabled="isLoading.value"
-							class="h-24 w-24 border"
-							:class="[
-								{
-									'bg-red-100':
-										selectedFruits.length === 3 &&
-										!isCorrectAnswer() &&
-										selectedFruits.includes(fruit),
-									'bg-green-100':
-										selectedFruits.length === 3 &&
-										isCorrectAnswer() &&
-										selectedFruits.includes(fruit),
-								},
-							]"
-							@click="selectFruit(fruit)"
-						/>
-					</div>
-				</div>
-			</div>
-			<div class="h-full w-full">
-				<HistorySection :history="reversedHistory" />
-			</div>
-		</div>
-		<GameOverSection
-			v-if="isGameOver"
-			:restart-game="restartGame"
-			:hidden-fruits="hiddenFruits"
-			:attempts-left="availableAttempts"
-			:selected-fruits="selectedFruits"
-		/>
-	</main>
+      <p>Hello Stranger 🕵️‍♂️</p>
+      <p>
+        Are you in search of the mystery fruits🍓? You’ve come to the right
+        place🌴. All you have to do is select the mystery fruits in the correct
+        order 1️⃣ 2️⃣ 3️⃣.
+      </p>
+      <p>
+        You have 6 attempts before the fruits are lost forever😞. You can check
+        the history tab on the left to see details on your previous
+        selections⌛.
+      </p>
+      <p>Good luck and Have fun🌟.</p>
+    </div>
+    <div
+      v-if="!isGameOver"
+      id="game"
+      class="grid lg:grid-cols-2 place-items-start justify-items-center content-center gap-4"
+    >
+      <div class="h-full w-full">
+        <div class="mt-4 text-pink-500 font-semibold">
+          Remaining Attempts:
+          <span class="font-black">{{ availableAttempts }}</span>
+        </div>
+        <div class="mt-4 grid grid-cols-3 gap-2 place-items-center">
+          <div
+            v-for="fruit in randomFruits"
+            :key="fruit"
+          >
+            <FruitButton
+              :fruit="fruit"
+              :selected-index="getSelectedFruitIndex(fruit)"
+              :disabled="isLoading.value"
+              class="h-24 w-24 border"
+              :class="[
+                {
+                  'bg-red-100':
+                    selectedFruits.length === 3 &&
+                    !isCorrectAnswer() &&
+                    selectedFruits.includes(fruit),
+                  'bg-green-100':
+                    selectedFruits.length === 3 &&
+                    isCorrectAnswer() &&
+                    selectedFruits.includes(fruit),
+                },
+              ]"
+              @click="selectFruit(fruit)"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="h-full w-full">
+        <HistorySection :history="reversedHistory" />
+      </div>
+    </div>
+    <GameOverSection
+      v-if="isGameOver"
+      :restart-game="restartGame"
+      :hidden-fruits="hiddenFruits"
+      :attempts-left="availableAttempts"
+      :selected-fruits="selectedFruits"
+    />
+  </main>
 </template>
