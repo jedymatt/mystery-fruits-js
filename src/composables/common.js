8950ad1e7df12b847b7556715e478bd65e60991c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-07 20:57:02 +0800

    Add useTheme method

--- src/composables/common.js
+++ src/composables/common.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 
 export const useInstruction = () => {
     const showInstructionAt = localStorage.getItem('common.show_instruction_at');
-    
+
     const showInstruction = ref(showInstructionAt < moment());
 
     function closeInstruction() {
@@ -14,5 +14,24 @@ export const useInstruction = () => {
     return {
         showInstruction,
         closeInstruction,
+    }
+}
+
+export const useTheme = () => {
+    function toggleDarkMode() {
+        localStorage.theme = localStorage.theme === 'dark' ? 'light' : 'dark';
+
+        // Apply the dark mode
+        const element = document.getElementsByTagName('html')[0];
+        if (element.classList.contains('dark')) {
+            element.classList.remove('dark')
+        } else {
+            element.classList.add('dark')
+        }
+        // document.documentElement.classList.toggle('dark');
+    }
+
+    return {
+        toggleDarkMode,
     }
 }
