4fd57a30cb408072d6fc6e16f7452c0991e31eb4
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-07 19:55:01 +0800

    Implement nprogress in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import NProgress from 'nprogress';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,5 +23,19 @@ const router = createRouter({
     ],
 })
 
+
+router.beforeResolve((to, from, next) => {
+    // If this isn't an initial page load.
+    if (to.name) {
+        // Start the route progress bar.
+        NProgress.start()
+    }
+    next()
+})
+
+router.afterEach(() => {
+    // Complete the animation of the route progress bar.
+    NProgress.done()
+})
 
 export default router;
