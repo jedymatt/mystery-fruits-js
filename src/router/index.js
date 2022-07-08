5df64cc5b2ff10f7336fd6d59886ccadeef3da8a
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-08 14:18:51 +0800

    Change from createWebHistory to createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import NProgress from 'nprogress';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
