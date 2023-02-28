4ad55ea5d4f32954788806e659af58ae3bcd3cdd
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-02-28 14:10:00 +0800

    Update default.vue

--- nuxt-app/layouts/default.vue
+++ nuxt-app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="antialiased font-sans text-gray-800 bg-pink-50">
+    <div class="antialiased font-sans text-gray-800 bg-pink-50 dark:bg-[#1c1a26] dark:text-pink-50">
         <AppNav />
         <main class="min-h-screen">
             <slot />
