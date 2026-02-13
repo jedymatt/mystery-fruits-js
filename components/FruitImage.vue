8603a7da5e33d05570f2822b81fba206aca128cf
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2026-02-13 18:33:25 +0800

    feat: add fruit image, nav pill, and link card components

---
+++ components/FruitImage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  fruit: string;
+}>();
+
+const imageUrl = new URL(`../assets/images/${props.fruit}.png`, import.meta.url).href;
+
+</script>
+
+<template>
+  <img
+    :src="imageUrl"
+    :alt="fruit"
+  >
+</template>
